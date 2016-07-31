7928ee469a41bf3d842ebe91be7efee357b247e8
chuiyuan <lisjthu@sina.com>
2016-07-31 23:16:29 +0800

    douban book crawler

--- routes/crawler.js
+++ routes/crawler.js
@@ -1,3 +1,40 @@
 /**
  * Created by chuiyuan on 16-7-31.
  */
+var cheerio = require('cheerio');
+var eventproxy = require('eventproxy');
+var crawlerLib = require('../lib/crawler');
+var express = require('express');
+var router = express.Router();
+var url = require('url');
+
+
+router.get('/', function (req, res, next) {
+    var render_res = res ;
+    var booklist = [];
+    var bookUrllist ;
+    var count =0;
+    
+    crawlerLib.getBookListUrl(function (result) {
+        bookUrllist = result;
+        for (var i=0; i<bookUrllist.length;i++){
+            crawlerLib.getUrlContent(bookUrllist[i], function (abook) {
+                var book = crawlerLib.getBookInfo(abook);
+                booklist.push(book);
+                count++;
+                console.log('has finished:'+count+', total:'+bookUrllist.length);
+            })
+        }
+        setInterval(function () {
+            if(count < bookUrllist.length){
+                console.log('yelp');
+                render_res.render('crawler',{
+                    title :'Crawler',
+                    booklsit : booklist
+                });
+            }
+        },2000);
+    })
+});
+
+module.exports = router;
